4e2d61865ed234a9cddc50065ef381ed0b17e955
Linards Biezbardis <linards@softikom.lv>
2018-03-20 17:11:23 +0200

    added WS server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import errorHandler from "errorhandler";
 
 import app from "./app";
+import WebSocket from "ws";
+import {stringify} from "querystring";
 
 /**
  * Error Handler. Provides full stack - remove for production
  */
 app.use(errorHandler());
+
+/**
+ * Start WebSocket server.
+ */
+const wss = new WebSocket.Server({ port: process.env.WS_PORT });
+
+wss.on("connection", function connection(ws: any) {
+    ws.on("message", function incoming(message: any) {
+        console.log("received %s", message);
+    });
+});
+
+export { wss as WebSocketServer };
 
 /**
  * Start Express server.
